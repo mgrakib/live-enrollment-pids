b6f7d22621c03993425b725bd9426dd21deb9239
mgrakib <mgrakibbd@gmail.com>
2023-04-30 16:37:00 +0600

    first commit

--- src/components/PrisonerData/Address.jsx
+++ src/components/PrisonerData/Address.jsx
@@ -3,7 +3,8 @@
 import React, { useEffect, useState } from "react";
 
 const Address = () => {
-	// this is for district
+
+	// this is for present district
 	const [district, setDistrict] = useState([]);
 	useEffect(() => {
 		fetch(`http://localhost:5000/district`)
@@ -14,15 +15,15 @@ const Address = () => {
 	// this is for present thana
 	const [presentThana, setPresentThana] = useState([]);
 
-	// select district for present 
+	// select district for present
 	const [searchPresentThana, setSearchPresentThana] = useState("Select");
 
 	// set function for present thana
 	const handelPresentThana = e => {
 		const district = e.target.value;
-		setSearchThana(district);
+		setSearchPresentThana(district);
 	};
-	// search preseents thanas 
+	// search preseents thanas
 	useEffect(() => {
 		fetch(`http://localhost:5000/district/${searchPresentThana}`)
 			.then(res => res.json())
@@ -30,22 +31,120 @@ const Address = () => {
 			.catch(error => setPresentThana([]));
 	}, [searchPresentThana]);
 
-	// for disableed 
-	const [isChecked, setIsChecked] = useState(false)
 
 
+	const [presentWard, setPresentWard] = useState([]);
+	const [searchPresentWard, setSearchPresentWard] = useState('Select');
+
+	const handelPresentWard = e => {
+		const thana = e.target.value;
+		setSearchPresentWard(thana);
+	}
+
+	useEffect(() => {
+		fetch(`http://localhost:5000/district/thana/${searchPresentWard}`)
+			.then(res => res.json())
+			.then(data => setPresentWard(data)).catch(error => setPresentWard([]));
+	},[searchPresentWard])
+
+
+const [presentWardValue, setPresentWardValue] = useState('Select');
+
+	const handelPresentWardValue = e => {
+		const ward = e.target.value;
+		setPresentWardValue(ward);
+	}
+
+	// for disableed
+	const [isChecked, setIsChecked] = useState(false);
+
+
+
+	// this is for present thana
+	const [permanentThana, setPermanentThana] = useState([]);
+	// select district for Permanent
+	const [searchPermanentThana, setSearchPermanentThana] = useState("Select");
+	// handel permanent thana
 	const handelPermanentThana = e => {
-		console.log(event.target.value);
-		
+		const permanentDistrict = event.target.value;
+		setSearchPermanentThana(permanentDistrict);
+	};
+
+	// search preseents thanas
+	useEffect(() => {
+		fetch(`http://localhost:5000/district/${searchPermanentThana}`)
+			.then(res => res.json())
+			.then(data => setPermanentThana(data))
+			.catch(error => setPermanentThana([]));
+	}, [searchPermanentThana]);
+
+
+	const [permanentWard, setPermanentWard] = useState([]);
+	const [searchPermanentWard, setSearchPermanentWard] = useState("Select");
+
+	const handelPermanentWard = e => {
+		const thana = e.target.value;
+		setSearchPermanentWard(thana);
+	};
+
+	useEffect(() => {
+		fetch(`http://localhost:5000/district/thana/${searchPermanentWard}`)
+			.then(res => res.json())
+			.then(data => setPermanentWard(data))
+			.catch(error => setPermanentWard([]));
+	}, [searchPermanentWard]);
+	
+
+	// PostOffice 
+	// present
+	const [presentPostOffice, setPresentPostOffice] = useState('')
+	const handelPresentPostOffice = e => {
+			setPresentPostOffice (e.target.value);
+	}
+	// Permanent 
+	const [permanentPostOffice, setPermanentPostOffice] = useState('');
+	const handelPermanentPostOffice = e => {
+		setPermanentPostOffice(e.target.value);
 	}
 
 
-
-
-
-
-
-
+	// Village 
+	// present
+	const [presentVillage, setPresentVillage] = useState('')
+	const handelPresentVillage = e => {
+			setPresentVillage (e.target.value);
+	}
+	// Permanent 
+	const [permanentVillage, setPermanentVillage] = useState('');
+	const handelPermanentVillage = e => {
+		setPermanentVillage(e.target.value);
+	}
+	
+	
+	// Street
+	// present
+	const [presentStreet, setPresentStreet] = useState('')
+	const handelPresentStreet = e => {
+			setPresentStreet (e.target.value);
+	}
+	// Permanent 
+	const [permanentStreet, setPermanentStreet] = useState('');
+	const handelPermanentStreet = e => {
+		setPermanentStreet(e.target.value);
+	}
+	
+	
+	// Home
+	// present
+	const [presentHome, setPresentHome] = useState('')
+	const handelPresentHome = e => {
+			setPresentHome (e.target.value);
+	}
+	// Permanent 
+	const [permanentHome, setPermanentHome] = useState('');
+	const handelPermanentHome = e => {
+		setPermanentHome(e.target.value);
+	}
 
 	return (
 		<div className='my-5'>
@@ -80,6 +179,7 @@ const Address = () => {
 							name=''
 							id=''
 							className='inputFeild'
+							onChange={handelPresentWard}
 							required
 						>
 							{presentThana.map((th, index) => (
@@ -101,35 +201,28 @@ const Address = () => {
 							id=''
 							className='inputFeild'
 							required
+							onChange={handelPresentWardValue}
 						>
-							{/* {bloodGroup.map((g, index) => (
+							{presentWard.map((th, index) => (
 								<option
 									key={index}
-									value={g}
+									value={th}
 								>
-									{g}
+									{th}
 								</option>
-							))} */}
+							))}
 						</select>
 					</div>
-					{/* post office  */}
+					{/* post office */}
 					<div>
-						<label htmlFor='nameEN'>post office</label>
-						<select
-							name=''
-							id=''
+						<label htmlFor='nameEN'>Post office</label>
+						<input
 							className='inputFeild'
+							name='postOffice'
 							required
-						>
-							{/* {bloodGroup.map((g, index) => (
-								<option
-									key={index}
-									value={g}
-								>
-									{g}
-								</option>
-							))} */}
-						</select>
+							onChange={handelPresentPostOffice}
+							value={presentPostOffice}
+						/>
 					</div>
 
 					{/* Village / Town */}
@@ -139,6 +232,8 @@ const Address = () => {
 							className='inputFeild'
 							name='banglaName'
 							required
+							onChange={handelPresentVillage}
+							value={presentVillage}
 						/>
 					</div>
 					{/* Street */}
@@ -148,6 +243,8 @@ const Address = () => {
 							className='inputFeild'
 							name='banglaName'
 							required
+							onChange={handelPresentStreet}
+							value={presentStreet}
 						/>
 					</div>
 					{/* House */}
@@ -157,11 +254,15 @@ const Address = () => {
 							className='inputFeild'
 							name='banglaName'
 							required
+							onChange={handelPresentHome}
+							value={presentHome}
 						/>
 					</div>
 				</div>
-				{/* Second column  */}
+
+				{/* Second column permanent  */}
 				<div className='flex flex-col gap-2'>
+					{/* check box  */}
 					<div className='flex justify-between'>
 						<p>PERMANENT</p>
 						<div>
@@ -176,7 +277,7 @@ const Address = () => {
 							Same as Present Address
 						</div>
 					</div>
-					{/* District  */}
+					{/* District permanent */}
 					<div>
 						<label htmlFor='nameEN'>District</label>
 						<select
@@ -190,19 +291,11 @@ const Address = () => {
 							{district.map((d, index) => (
 								<option
 									key={index}
-									value={d}
-								>
-									{d}
-								</option>
-							))}
-							{/* {district.map((d, index) => (
-								<option
-									key={index}
 									value={isChecked ? searchPresentThana : d}
 								>
 									{isChecked ? searchPresentThana : d}
 								</option>
-							))} */}
+							))}
 						</select>
 					</div>
 					{/* Thana  */}
@@ -213,15 +306,17 @@ const Address = () => {
 							id=''
 							className='inputFeild'
 							required
+							disabled={isChecked}
+							onChange={handelPermanentWard}
 						>
-							{/* {bloodGroup.map((g, index) => (
+							{permanentThana.map((th, index) => (
 								<option
 									key={index}
-									value={g}
+									value={isChecked ? searchPresentWard : th}
 								>
-									{g}
+									{isChecked ? searchPresentWard : th}
 								</option>
-							))} */}
+							))}
 						</select>
 					</div>
 
@@ -233,35 +328,33 @@ const Address = () => {
 							id=''
 							className='inputFeild'
 							required
+							disabled={isChecked}
 						>
-							{/* {bloodGroup.map((g, index) => (
+							{permanentWard.map((wa, index) => (
 								<option
 									key={index}
-									value={g}
+									value={isChecked ? presentWardValue : wa}
 								>
-									{g}
+									{isChecked ? presentWardValue : wa}
 								</option>
-							))} */}
+							))}
 						</select>
 					</div>
-					{/* post office  */}
+					{/* post office */}
 					<div>
-						<label htmlFor='nameEN'>post office</label>
-						<select
-							name=''
-							id=''
+						<label htmlFor='nameEN'>Post office</label>
+						<input
 							className='inputFeild'
+							name='postOffice'
 							required
-						>
-							{/* {bloodGroup.map((g, index) => (
-								<option
-									key={index}
-									value={g}
-								>
-									{g}
-								</option>
-							))} */}
-						</select>
+							value={
+								isChecked
+									? presentPostOffice
+									: permanentPostOffice
+							}
+							disabled={isChecked}
+							onChange={handelPermanentPostOffice}
+						/>
 					</div>
 
 					{/* Village / Town */}
@@ -271,6 +364,11 @@ const Address = () => {
 							className='inputFeild'
 							name='banglaName'
 							required
+							value={
+								isChecked ? presentVillage : permanentVillage
+							}
+							disabled={isChecked}
+							onChange={handelPermanentVillage}
 						/>
 					</div>
 					{/* Street */}
@@ -280,6 +378,9 @@ const Address = () => {
 							className='inputFeild'
 							name='banglaName'
 							required
+							value={isChecked ? presentStreet : permanentStreet}
+							disabled={isChecked}
+							onChange={handelPermanentStreet}
 						/>
 					</div>
 					{/* House */}
@@ -289,6 +390,9 @@ const Address = () => {
 							className='inputFeild'
 							name='banglaName'
 							required
+							value={isChecked ? presentHome : permanentHome}
+							disabled={isChecked}
+							onChange={handelPermanentHome}
 						/>
 					</div>
 				</div>
